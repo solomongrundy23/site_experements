ebd56da2f04ea6de401e8495b5a61eda4d5e00f7
Анисимов Сергей Юрьевич <s.anisimov@csat.ru>
2019-10-24 11:30:14 +0300

    core remove - request repaired in app.js

--- app.js
+++ app.js
@@ -12,11 +12,8 @@ const positionRoutes = require('./routes/position')
 const keys = require('./config/keys')
 
 mongoose.connect(keys.mongoURI).then(() => console.log('MongoDB connected')).catch(error => console.log(`Error ${error}`))
-
 app.use(require('morgan')('dev'))
-app.use(require('cors'))
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json)
+
 app.use('/api/auth', authRoutes)
 app.use('/api/analytics', analyticsRoutes)
 app.use('/api/category', categoryRoutes)
